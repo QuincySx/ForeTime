92d55f24fbb77f407330eba403e62a94d7d2c749
QuincySx <772804430@qq.com>
2018-09-17 17:20:41 +0800

    first commit

---
+++ app/src/main/java/com/smallraw/foretime/app/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.smallraw.foretime.app
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
