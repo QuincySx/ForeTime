5f9922d609cc9f5aebd72fa3198d00df969c9236
QuincySx <772804430@qq.com>
2018-10-29 21:00:35 +0800

    add 添加音乐列表

---
+++ app-foretime/src/main/java/com/smallraw/foretime/app/ui/musicListActivity/MusicBean.java
@@ -0,0 +1,18 @@
+package com.smallraw.foretime.app.ui.musicListActivity;
+
+public class MusicBean {
+  public String image;
+  public String musicUrl;
+  public String title;
+  public String content;
+
+  public MusicBean() {
+  }
+
+  public MusicBean(String image, String musicUrl, String title, String content) {
+    this.image = image;
+    this.musicUrl = musicUrl;
+    this.title = title;
+    this.content = content;
+  }
+}
---
+++ app-foretime/src/main/java/com/smallraw/foretime/app/ui/musicListActivity/MusicListActivity.kt
@@ -0,0 +1,43 @@
+package com.smallraw.foretime.app.ui.musicListActivity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import com.smallraw.foretime.app.App
+import com.smallraw.foretime.app.R
+import com.smallraw.time.base.BaseTitleBarActivity
+import com.smallraw.time.model.thoroughDeleteTaskAll
+import kotlinx.android.synthetic.main.activity_music_list.*
+import me.jessyan.autosize.AutoSizeConfig
+import me.jessyan.autosize.utils.AutoSizeUtils
+
+class MusicListActivity : BaseTitleBarActivity() {
+    private val mMusicList = ArrayList<MusicBean>();
+    private val mAdapter = MusicAdapter(mMusicList);
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_music_list)
+        setTitleBarLeftImage(R.drawable.ic_back_black)
+        addRightView(newTitleRightView())
+
+        mMusicList.add(MusicBean("","","测试","我是测试啊"))
+        mMusicList.add(MusicBean("","","测试","我是测试啊"))
+
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        recyclerViewMusic.layoutManager = LinearLayoutManager(this)
+        recyclerViewMusic.adapter = mAdapter
+    }
+
+    private fun newTitleRightView(): View {
+        val inflate = layoutInflater.inflate(R.layout.layout_harvest_number, null)
+        inflate.setPadding(0, 0, AutoSizeUtils.dp2px(this, 12F), 0)
+        return inflate
+    }
+}
